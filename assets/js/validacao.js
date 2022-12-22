03fc8f92589ce11e41d557b599c566f82599c1ff
Gabriel Mendes Rangel <gabrielmendesrangel2@gmail.com>
2022-12-22 11:39:52 -0300

    Initial commit

---
+++ assets/js/validacao.js
@@ -0,0 +1,29 @@
+export function valida(input){
+    const tipoInput = input.dataset.tipo;
+
+    if(validadores[tipoInput]){
+        validadores[tipoInput](input);
+    };
+};
+
+const validadores = {
+    dataNascimento:input => validaDataNascimento(input) 
+};
+
+function validaDataNascimento(input){
+    const dataRecebida = new Date(input.value);
+    let mensagem = "";
+
+    if(!maiorQue18(dataRecebida)){
+        mensagem = "Você deve ver maior de idade para se cadastrar."
+    };
+
+    input.setCustomValidity(mensagem);
+};
+
+function maiorQue18(data){
+    const dataAtual = new Date();
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
+
+    return dataMais18 <= dataAtual;
+};
